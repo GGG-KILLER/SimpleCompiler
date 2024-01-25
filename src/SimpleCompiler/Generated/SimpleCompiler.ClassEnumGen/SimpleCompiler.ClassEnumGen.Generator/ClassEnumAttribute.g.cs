483d9e0349801b17220c5ac59526ac83a31fd4f8
GGG <gggkiller2@gmail.com>
2024-01-25 05:39:27 -0300

    Add generated files.

---
+++ src/SimpleCompiler/Generated/SimpleCompiler.ClassEnumGen/SimpleCompiler.ClassEnumGen.Generator/ClassEnumAttribute.g.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+#nullable enable
+
+namespace ClassEnumGen;
+
+[AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+public sealed class ClassEnumAttribute : Attribute
+{
+    public string? KindEnumName { get; set; }
+}
