cecc53c99d62877b552f5c47d20f8947e90ac168
GGG <gggkiller2@gmail.com>
2024-01-26 01:44:44 -0300

    Make ExpressionHelper error earlier.

--- src/SimpleCompiler/Helpers/ExpressionHelper.cs
+++ src/SimpleCompiler/Helpers/ExpressionHelper.cs
@@ -5,12 +5,14 @@ namespace SimpleCompiler.Helpers;
 
 public class ExpressionHelper
 {
-    public static MethodInfo MethodInfo(Expression<Action> expression) => (expression.Body as MethodCallExpression)!.Method;
-    public static MethodInfo MethodInfo<T>(Expression<Action<T>> expression) => (expression.Body as MethodCallExpression)!.Method;
-    public static MethodInfo MethodInfo<T>(Expression<Func<T>> expression) => (expression.Body as MethodCallExpression)!.Method;
-    public static PropertyInfo PropertyInfo(Expression<Func<object>> expression) => ((expression.Body as MemberExpression)!.Member as PropertyInfo)!;
-    public static PropertyInfo PropertyInfo<T>(Expression<Func<T, object>> expression) => ((expression.Body as MemberExpression)!.Member as PropertyInfo)!;
-    public static FieldInfo FieldInfo(Expression<Func<object>> expression) => ((expression.Body as MemberExpression)!.Member as FieldInfo)!;
-    public static FieldInfo FieldInfo<T>(Expression<Func<T, object>> expression) => ((expression.Body as MemberExpression)!.Member as FieldInfo)!;
-    public static ConstructorInfo ConstructorInfo<T>(Expression<Func<T>> expression) => (expression.Body as NewExpression)!.Constructor!;
+    public static MethodInfo MethodInfo(Expression<Action> expression) => ((MethodCallExpression)expression.Body)!.Method;
+    public static MethodInfo MethodInfo<T>(Expression<Action<T>> expression) => ((MethodCallExpression)expression.Body)!.Method;
+    public static MethodInfo MethodInfo<T>(Expression<Func<T>> expression) => ((MethodCallExpression)expression.Body)!.Method;
+    public static PropertyInfo PropertyInfo(Expression<Func<object>> expression) => (PropertyInfo)((MemberExpression)expression.Body).Member;
+    public static PropertyInfo PropertyInfo<T>(Expression<Func<T, object>> expression) => (PropertyInfo)((MemberExpression)expression.Body).Member;
+    public static MethodInfo PropertyGet(Expression<Func<object>> expression) => ((PropertyInfo)((MemberExpression)expression.Body).Member).GetMethod!;
+    public static MethodInfo PropertyGet<T>(Expression<Func<T, object>> expression) => ((PropertyInfo)((MemberExpression)expression.Body).Member).GetMethod!;
+    public static FieldInfo FieldInfo<T>(Expression<Func<T>> expression) => (FieldInfo)((MemberExpression)expression.Body).Member;
+    public static FieldInfo FieldInfo<T>(Expression<Func<T, object>> expression) => (FieldInfo)((MemberExpression)expression.Body).Member;
+    public static ConstructorInfo ConstructorInfo<T>(Expression<Func<T>> expression) => ((NewExpression)expression.Body)!.Constructor!;
 }
