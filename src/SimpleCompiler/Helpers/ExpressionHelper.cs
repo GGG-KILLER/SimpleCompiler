05832ad1d8b22a2c9a0f4eae569260a5ca18c505
GGG <gggkiller2@gmail.com>
2024-01-26 00:38:45 -0300

    Add more helpers.

--- src/SimpleCompiler/Helpers/ExpressionHelper.cs
+++ src/SimpleCompiler/Helpers/ExpressionHelper.cs
@@ -6,5 +6,11 @@ namespace SimpleCompiler.Helpers;
 public class ExpressionHelper
 {
     public static MethodInfo MethodInfo(Expression<Action> expression) => (expression.Body as MethodCallExpression)!.Method;
+    public static MethodInfo MethodInfo<T>(Expression<Action<T>> expression) => (expression.Body as MethodCallExpression)!.Method;
     public static MethodInfo MethodInfo<T>(Expression<Func<T>> expression) => (expression.Body as MethodCallExpression)!.Method;
+    public static PropertyInfo PropertyInfo(Expression<Func<object>> expression) => ((expression.Body as MemberExpression)!.Member as PropertyInfo)!;
+    public static PropertyInfo PropertyInfo<T>(Expression<Func<T, object>> expression) => ((expression.Body as MemberExpression)!.Member as PropertyInfo)!;
+    public static FieldInfo FieldInfo(Expression<Func<object>> expression) => ((expression.Body as MemberExpression)!.Member as FieldInfo)!;
+    public static FieldInfo FieldInfo<T>(Expression<Func<T, object>> expression) => ((expression.Body as MemberExpression)!.Member as FieldInfo)!;
+    public static ConstructorInfo ConstructorInfo<T>(Expression<Func<T>> expression) => (expression.Body as NewExpression)!.Constructor!;
 }
