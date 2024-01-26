9b0a2fdcb31be8815c39b5753deed87849beab00
GGG <gggkiller2@gmail.com>
2024-01-26 00:36:20 -0300

    Add new static class to store the builtin globals.

---
+++ src/SimpleCompiler.Runtime/StockGlobals.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleCompiler.Runtime;
+
+public static class StockGlobals
+{
+    [SuppressMessage("Code Quality", "CA2211", Justification = "Not gonna make the runtime use properties unnecessarily.")]
+    public static LuaValue Print = new(Stdlib.Print);
+
+    [SuppressMessage("Code Quality", "CA2211", Justification = "Not gonna make the runtime use properties unnecessarily.")]
+    public new static LuaValue ToString = new(Stdlib.ToString);
+}
