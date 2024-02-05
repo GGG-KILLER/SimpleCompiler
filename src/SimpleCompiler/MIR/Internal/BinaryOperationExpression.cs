dc916594d53ee88cbcb9f847bb6de46a99834621
GGG <gggkiller2@gmail.com>
2024-02-05 02:25:37 -0300

    Remove ordering as new version doesn't need it.

--- src/SimpleCompiler/MIR/Internal/BinaryOperationExpression.cs
+++ src/SimpleCompiler/MIR/Internal/BinaryOperationExpression.cs
@@ -5,7 +5,6 @@ namespace SimpleCompiler.MIR.Internal;
 [GreenNode(MirKind.BinaryOperationExpression)]
 internal sealed partial class BinaryOperationExpression : Expression
 {
-    [NodeComponent(Order = 0)]
     private readonly BinaryOperationKind _binaryOperationKind;
     private readonly Expression _left;
     private readonly Expression _right;
--- src/SimpleCompiler/MIR/Internal/UnaryOperation.cs
+++ src/SimpleCompiler/MIR/Internal/UnaryOperation.cs
@@ -5,7 +5,6 @@ namespace SimpleCompiler.MIR.Internal;
 [GreenNode(MirKind.UnaryOperationExpression)]
 internal sealed partial class UnaryOperationExpression : Expression
 {
-    [NodeComponent(Order = 0)]
     private readonly UnaryOperationKind _unaryOperationKind;
     private readonly Expression _operand;
 }
