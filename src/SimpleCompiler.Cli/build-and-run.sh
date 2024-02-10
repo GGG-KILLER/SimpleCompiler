962b928aaf08b0d3b71970aa7c7bb379f3251919
GGG <gggkiller2@gmail.com>
2024-02-10 20:43:18 -0300

    Improve mass build and run script.

--- src/SimpleCompiler.Cli/build-and-run.sh
+++ src/SimpleCompiler.Cli/build-and-run.sh
@@ -1,12 +1,30 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
+OPTIMIZE=false
+if [ "$1" = "-O" ]; then
+    shift 1
+    OPTIMIZE=true
+fi
+
 while [ $# -gt 0 ]; do
-    FILE="$1"
+    FILE_DIR=$(dirname "$1")
+    FILE_NAME=$(basename -s.lua "$1")
     shift 1
 
-    rm "${FILE/.lua/}".{dll,*.mir,cil,runtimeconfig.json,*.lua} || true;
-    echo "$FILE:"
-    dotnet run -c Debug -v quiet -- build -d "$FILE" 2>&1 | sed -e 's/^/  build: /'
-    dotnet "${FILE/.lua/.dll}" 2>&1 | sed -e 's/^/  run: /'
+    for trash in "$FILE_DIR/obj/$FILE_NAME".*; do
+        if [ -e "$trash" ]; then
+            rm "$trash"
+        fi
+    done
+
+    if $OPTIMIZE; then
+        echo "$FILE_DIR/$FILE_NAME.lua (Release):"
+        dotnet run -c Debug -v quiet -- -Od "$FILE_DIR/$FILE_NAME.lua" 2>&1 | sed -e 's/^/  build: /'
+        dotnet "$FILE_DIR/$FILE_NAME.dll" 2>&1 | sed -e 's/^/  run: /'
+    else
+        echo "$FILE_DIR/$FILE_NAME.lua (Debug):"
+        dotnet run -c Debug -v quiet -- -d "$FILE_DIR/$FILE_NAME.lua" 2>&1 | sed -e 's/^/  build: /'
+        dotnet "$FILE_DIR/$FILE_NAME.dll" 2>&1 | sed -e 's/^/  run: /'
+    fi
 done
