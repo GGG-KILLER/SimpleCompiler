9f935a5c3cf2cce8ff48632fe8c5f46c21448546
GGG <gggkiller2@gmail.com>
2024-02-10 20:14:57 -0300

    Add OperationFacts.

--- src/SimpleCompiler/IR/BinaryOperationKind.cs
+++ src/SimpleCompiler/IR/BinaryOperationKind.cs
@@ -26,3 +26,26 @@ public enum BinaryOperationKind
     GreaterThan,
     GreaterThanOrEquals,
 }
+
+public static class BinaryOperationKindExtensions
+{
+    public static bool IsArithmetic(this BinaryOperationKind kind) =>
+        kind is BinaryOperationKind.Addition or BinaryOperationKind.Subtraction
+             or BinaryOperationKind.Multiplication or BinaryOperationKind.Division
+             or BinaryOperationKind.Exponentiation or BinaryOperationKind.Modulo
+             or BinaryOperationKind.IntegerDivision;
+
+    public static bool IsBitwise(this BinaryOperationKind kind) =>
+        kind is BinaryOperationKind.BitwiseAnd or BinaryOperationKind.BitwiseOr
+             or BinaryOperationKind.LeftShift or BinaryOperationKind.RightShift
+             or BinaryOperationKind.BitwiseXor;
+
+    public static bool IsNumericComparison(this BinaryOperationKind kind) =>
+        kind is BinaryOperationKind.LessThan or BinaryOperationKind.LessThanOrEquals
+             or BinaryOperationKind.GreaterThan or BinaryOperationKind.GreaterThanOrEquals;
+
+    public static bool IsComparison(this BinaryOperationKind kind) =>
+        kind is BinaryOperationKind.Equals or BinaryOperationKind.NotEquals
+             or BinaryOperationKind.LessThan or BinaryOperationKind.LessThanOrEquals
+             or BinaryOperationKind.GreaterThan or BinaryOperationKind.GreaterThanOrEquals;
+}
