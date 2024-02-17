56f618e23e5668527e5c3efa24e4dcdbbe0ce60e
GGG <gggkiller2@gmail.com>
2024-02-17 20:07:54 -0300

    Rewrite NameValue to support unversioned and temporary names.

--- src/SimpleCompiler/IR/NameValue.cs
+++ src/SimpleCompiler/IR/NameValue.cs
@@ -4,11 +4,17 @@ namespace SimpleCompiler.IR;
 
 public sealed class NameValue(string name, int version) : Operand
 {
-    public const string TemporaryName = "t";
+    private const string TemporaryName = "t";
+    private const int UnversionedVersion = -1;
+
     public static NameValue Temporary(int version) => new(TemporaryName, version);
+    public static NameValue Unversioned(string name) => new(name, UnversionedVersion);
 
     public string Name { get; } = name;
     public int Version { get; } = version;
 
-    public override string ToString() => $"{Name}{Version.ToSubscript()}";
+    public bool IsTemporary => Name == TemporaryName;
+    public bool IsUnversioned => Version == UnversionedVersion;
+
+    public override string ToString() => IsUnversioned ? Name : $"{Name}{Version.ToSubscript()}";
 }
