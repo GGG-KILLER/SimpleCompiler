e194b6ce14ed359cb3baea1ecb9d91e84c13cc8b
GGG <gggkiller2@gmail.com>
2024-02-22 04:24:25 -0300

    Add per-type slot pools.

--- src/SimpleCompiler/Backends/Cil/SlotPool.cs
+++ src/SimpleCompiler/Backends/Cil/SlotPool.cs
@@ -1,19 +1,34 @@
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using Sigil;
+using SimpleCompiler.Helpers;
 using SimpleCompiler.Runtime;
 
 namespace SimpleCompiler.Backends.Cil;
 
-internal sealed class SlotPool(Emit<Func<LuaValue, LuaValue>> method)
+internal sealed class SlotPool
 {
     private static readonly ConditionalWeakTable<Local, SlotPool> s_localOwners = [];
-    private readonly Stack<Local> _luaValueSlotPool = [];
+    private static readonly ConditionalWeakTable<Local, object> s_localTypes = [];
+    private readonly Stack<Local>[] _luaValueSlotPool;
+    private readonly Emit<Func<LuaValue, LuaValue>> _method;
 
-    public Local DangerousRentSlot()
+    public SlotPool(Emit<Func<LuaValue, LuaValue>> method)
     {
-        if (!_luaValueSlotPool.TryPop(out var local))
+        _method = method;
+        _luaValueSlotPool = new Stack<Local>[5];
+        for (var idx = 0; idx < _luaValueSlotPool.Length; idx++)
+            _luaValueSlotPool[idx] = new Stack<Local>();
+    }
+
+    public Local DangerousRentSlot(LocalType type)
+    {
+        if (type == LocalType.None)
+            throw new ArgumentException("None is not a valid type for a local.", nameof(type));
+
+        if (!_luaValueSlotPool[(int) type].TryPop(out var local))
         {
-            local = method.DeclareLocal<LuaValue>();
+            local = _method.DeclareLocal(type.GetClrType());
         }
 
         s_localOwners.Add(local, this);
@@ -26,23 +41,56 @@ internal sealed class SlotPool(Emit<Func<LuaValue, LuaValue>> method)
             throw new InvalidOperationException("Slot returned to wrong owner.");
         s_localOwners.Remove(local);
 
+        LocalType type = LocalType.None;
+        if (s_localTypes.TryGetValue(local, out var boxedType))
+            type = CastHelper.FastUnbox<LocalType>(boxedType);
+        if (type == LocalType.None)
+            return;
+
         // Even though we shouldn't grow unlimitedly as a pool,
         // we also don't want to infinitely create locals, so
         // keep every single local we create to avoid creating
         // new ones whenever possible.
-        _luaValueSlotPool.Push(local);
+        _luaValueSlotPool[(int) type].Push(local);
     }
 
-    public void WithSlot(Action<Emit<Func<LuaValue, LuaValue>>, Local> body)
+    public void WithSlot(LocalType type, Action<Emit<Func<LuaValue, LuaValue>>, Local> body)
     {
-        var local = DangerousRentSlot();
+        var local = DangerousRentSlot(type);
         try
         {
-            body(method, local);
+            body(_method, local);
         }
         finally
         {
             ReturnSlot(local);
         }
     }
+}
+
+internal enum LocalType
+{
+    None = 0,
+
+    Bool,
+    Long,
+    Double,
+    String,
+    LuaFunction,
+    LuaValue,
+}
+
+internal static class LocalTypeExtensions
+{
+    public static Type GetClrType(this LocalType type) =>
+        type switch
+        {
+            LocalType.Bool => typeof(bool),
+            LocalType.Long => typeof(long),
+            LocalType.Double => typeof(double),
+            LocalType.String => typeof(string),
+            LocalType.LuaFunction => typeof(LuaFunction),
+            LocalType.LuaValue => typeof(LuaValue),
+            _ => throw new UnreachableException()
+        };
 }
