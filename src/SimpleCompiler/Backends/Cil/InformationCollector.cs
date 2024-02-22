31bfc0cd5be0e96e385b815fc24839e6be7e507b
GGG <gggkiller2@gmail.com>
2024-02-22 04:24:46 -0300

    Add a symbol table and an information collector.

---
+++ src/SimpleCompiler/Backends/Cil/InformationCollector.cs
@@ -0,0 +1,69 @@
+using SimpleCompiler.Helpers;
+using SimpleCompiler.IR;
+
+namespace SimpleCompiler.Backends.Cil;
+
+internal static class InformationCollector
+{
+    public static SymbolTable CollectSymbolInfomation(IrGraph graph)
+    {
+        var table = new SymbolTable();
+        foreach (var block in graph.EnumerateBlocksBreadthFirst().Select(x => graph.BasicBlocks[x]))
+        {
+            foreach (var instruction in block.Instructions)
+            {
+                switch (instruction.Kind)
+                {
+                    case InstructionKind.Assignment:
+                    {
+                        var assignment = CastHelper.FastCast<Assignment>(instruction);
+                        table[assignment.Name].Types = GetOperandType(assignment.Value, table);
+                        break;
+                    }
+                    case InstructionKind.UnaryAssignment:
+                    {
+                        var assignment = CastHelper.FastCast<UnaryAssignment>(instruction);
+                        table[assignment.Name].Types = OperationFacts.GetOperationOutput(assignment.OperationKind, GetOperandType(assignment.Operand, table));
+                        break;
+                    }
+                    case InstructionKind.BinaryAssignment:
+                    {
+                        var assignment = CastHelper.FastCast<BinaryAssignment>(instruction);
+                        table[assignment.Name].Types = OperationFacts.GetOperationOutput(
+                            assignment.OperationKind,
+                            GetOperandType(assignment.Left, table),
+                            GetOperandType(assignment.Right, table));
+                        break;
+                    }
+                    case InstructionKind.FunctionAssignment:
+                        table[instruction.Name].Types = SymbolType.All;
+                        break;
+                    case InstructionKind.PhiAssignment:
+                    {
+                        var assignment = CastHelper.FastCast<PhiAssignment>(instruction);
+                        table[assignment.Name].Types = assignment.Phi.Values.Aggregate(SymbolType.None, (acc, value) => acc | table[value.Value].Types);
+                        break;
+                    }
+                }
+            }
+        }
+        return table;
+    }
+
+    public static SymbolType GetOperandType(Operand operand, SymbolTable table) =>
+        operand switch
+        {
+            Constant constant => constant.Value switch
+            {
+                bool => SymbolType.Boolean,
+                double => SymbolType.Double,
+                long => SymbolType.Long,
+                string => SymbolType.String,
+                null => SymbolType.Nil,
+                _ => throw new NotImplementedException()
+            },
+            Builtin => SymbolType.Function,
+            NameValue name => table[name].Types,
+            _ => throw new NotImplementedException()
+        };
+}
---
+++ src/SimpleCompiler/Backends/Cil/SymbolTable.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using SimpleCompiler.IR;
+
+namespace SimpleCompiler.Backends.Cil;
+
+internal sealed class SymbolTable : IReadOnlyDictionary<NameValue, SymbolData>
+{
+    private readonly Dictionary<NameValue, SymbolData> _data = [];
+
+    public SymbolData this[NameValue name] => _data[name] ??= new SymbolData(name);
+    public IEnumerable<NameValue> Keys => _data.Keys;
+    public IEnumerable<SymbolData> Values => _data.Values;
+    public int Count => _data.Count;
+
+    public bool TryGetValue(NameValue key, [MaybeNullWhen(false)] out SymbolData value) => _data.TryGetValue(key, out value);
+    public bool ContainsKey(NameValue key) => _data.ContainsKey(key);
+    public IEnumerator<KeyValuePair<NameValue, SymbolData>> GetEnumerator() => _data.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal sealed class SymbolData(NameValue name)
+{
+    public NameValue Name { get; } = name;
+    public SymbolType Types { get; set; } = SymbolType.All;
+    public LocalType LocalType => Types switch
+    {
+        SymbolType.Boolean => LocalType.Bool,
+        SymbolType.Long => LocalType.Long,
+        SymbolType.Double => LocalType.Double,
+        SymbolType.String => LocalType.String,
+        SymbolType.Function => LocalType.LuaFunction,
+        _ => LocalType.LuaValue,
+    };
+
+    public override string ToString() => $"{Name} (Types: [{Types}], Local Type: {LocalType})";
+    private string GetDebuggerDisplay() => ToString();
+}
+
+[Flags]
+internal enum SymbolType : byte
+{
+    None = 0,
+    Nil = 1 << 1,
+    Boolean = 1 << 2,
+    Long = 1 << 3,
+    Double = 1 << 4,
+    String = 1 << 5,
+    Function = 1 << 6,
+
+    All = Nil | Boolean | Long | Double | String | Function,
+}
+
+internal static class SymbolTypeExtensions
+{
+    public static bool IsMixed(this SymbolType type) =>
+        type is not (SymbolType.Nil or SymbolType.Boolean or SymbolType.Long or SymbolType.Double or SymbolType.String or SymbolType.Function);
+}
